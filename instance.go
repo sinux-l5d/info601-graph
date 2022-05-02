6e271c3b8e581409215ba410a6ec051bfcdceb8d
Simon LEONARD <git-1001af4@sinux.sh>
2022-05-02 17:29:50 +0200

    même si déjà fini, un peu plus propre...

--- instance.go
+++ instance.go
@@ -19,42 +19,10 @@ func NewInstanceOf(name string, concept *Concept) *Instance {
 	return i
 }
 
-func (c *Instance) GetName() string {
-	return c.Node.GetName()
-}
-
-func (c *Instance) AddRelationship(name string, node INode) {
-	c.Node.AddRelationship(name, node)
-}
-
-func (c *Instance) AddRelationshipBidirectional(name string, node INode) {
-	c.Node.AddRelationshipBidirectional(name, node)
-}
-
-func (c *Instance) Get(name string) []INode {
-	return c.Node.Get(name)
-}
-
 func (c *Instance) Print() {
 	c.Node.Print()
 	fmt.Println("Attributes:")
 	for k, v := range c.Node.attribute {
 		fmt.Println("\t", k, ":", v)
 	}
-}
-
-func (c *Instance) AUnCheminVers(node INode, relation string) bool {
-	return c.Node.AUnCheminVers(node, relation)
-}
-
-func (c *Instance) CheminProfondeurVers(node INode, relation string) ([]INode, bool) {
-	return c.Node.CheminProfondeurVers(node, relation)
-}
-
-func (c *Instance) CheminAccessible(relation string) map[string]Origine {
-	return c.Node.CheminAccessible(relation)
-}
-
-func (c *Instance) CheminOptiVers(node INode, relation string) ([]string, bool) {
-	return c.Node.CheminOptiVers(node, relation)
 }
